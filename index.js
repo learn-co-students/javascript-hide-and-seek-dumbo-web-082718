535751c1ba9d433ed4f920f76927e7776ce58341
jordanmmartin <jdordan23@gmail.com>
2018-10-11 10:09:04 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 
+function getFirstSelector(selector) {
+  return document.querySelector(selector);
+}
+
+function nestedTarget() {
+  let div = document.querySelector('#nested');
+  return div.querySelector('.target')
+}
+
+function increaseRankBy(n) {
+  let lis = document.querySelectorAll('ul.ranked-list li');
+  lis.forEach(function(li) {
+    li.innerText = parseInt(li.innerText, 10) + n;
+  })
+}
+
+function deepestChild() {
+  let grandNode = document.querySelector('#grand-node');
+  let divs = grandNode.querySelectorAll('div');
+  return divs[divs.length - 1];
+}
